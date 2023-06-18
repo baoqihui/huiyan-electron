72d8d8fbbd5b87167ec94406694c61b972b79f77
回宝旗 <huibq@cheche365.com>
2023-06-18 23:39:19 +0800

    图标圆角,关于按钮

--- main.js
+++ main.js
@@ -1,35 +1,83 @@
-const { app, BrowserWindow } = require('electron')
+const {app, Menu, BrowserWindow, ipcMain, session, shell} = require('electron')
+
 const path = require('path');
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 1200,
-    height: 900,
-    icon: path.join(__dirname,"./assets/background.ico"),
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true
-    }
-  })
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 900,
+        icon: path.join(__dirname, "./assets/background.ico"),
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
+    })
+    const menuTemplate = [
+        {
+            label: '操作',
+            submenu: [
+                {
+                    label: '清除缓存',
+                    click: async () => {
+                        console.log('start clear cache')
+                        session.defaultSession.clearCache().then(() => {
+                        }).catch((error) => {
+                        })
 
-  win.loadURL(`https://onechat.huiyan-ai.com/`)
-  win.setMenuBarVisibility(false)
+                        session.defaultSession.clearStorageData({
+                            storages: ['cookies', 'filesystem', 'indexdb', 'websql', 'shadercache', 'localstorage', 'serviceworkers', 'cachestorage']
+                        }).then(() => {
+                        }).catch((error) => {
+                        })
 
+                        win.webContents.session.clearCache().then(() => {
+                            win.reload()
+                        }).catch(error => {
+                        })
+                    }
+                },
+            ]
+        }, {
+            label: '关于',
+            submenu: [
+                {
+                    label: '慧言官网',
+                    click: async () => {
+                        await shell.openExternal('https://www.huiyan-ai.com')
+                    }
+                },
+                {
+                    label: '联系我们',
+                    click: async () => {
+                        await shell.openExternal('https://www.huiyan-ai.com/qun.html')
+                    }
+                },
+                {
+                    label: '版本号：0.5.5'
+                }
+            ]
+        }
+    ]
+
+    const menu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(menu)
+    win.loadURL(`https://onechat.huiyan-ai.com/`)
+    // win.setMenuBarVisibility(false)
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
